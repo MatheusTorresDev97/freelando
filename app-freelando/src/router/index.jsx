8b94a9c35570cb9071400c5d5e3907719b3cedce
Matheus <matheustorresdev@gmail.com>
2023-11-24 20:46:09 -0300

    finalizando a tela inicial

--- app-freelando/src/router/index.jsx
+++ app-freelando/src/router/index.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import LayoutBase from "../paginas/LayoutBase";
+import PaginaInicial from "../paginas/PaginaInicial";
 
 export const router = createBrowserRouter([
     {
         path:"/",
         element: <LayoutBase />,
         ErrorBoundary: <h1>Página Nao Encontrada</h1>,
+        children: [
+            {
+                path: "",
+                element: <PaginaInicial />
+            },
+        ]
     }
 ])
