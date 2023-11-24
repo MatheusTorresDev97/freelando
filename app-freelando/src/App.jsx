f29e1da71d03a6438cb1cf634d99d0548030728b
Matheus <matheustorresdev@gmail.com>
2023-11-24 20:13:35 -0300

    Estilos Globais e Reset

--- app-freelando/src/App.jsx
+++ app-freelando/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { ProvedorTema } from './components/ProvedorTema'
+import { Estilos } from "./componentes/EstilosGlobais/Estilos";
 
 const App = () => {
   return (
-    <ProvedorTema>App</ProvedorTema>
+    <ProvedorTema>
+      <Estilos />
+    </ProvedorTema>
   )
 }
 
